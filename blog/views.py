4225600d4101431a1373c899de2ff961ba33a1cd
Prasanth Karri <prasanth.25k@gmail.com>
2020-02-22 16:19:48 +0530

    add blog detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -6,3 +6,8 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'posts'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
